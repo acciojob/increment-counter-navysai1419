bc42fe69ef95e33d8680054d903572d8318dda20
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-08 14:05:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
     function increment() {
         // Display the un-incremented value in an alert
-        alert("The un-incremented value is: " + counterValue);
+        alert(counterValue);
 
         // Increment the counter value
         counterValue++;
