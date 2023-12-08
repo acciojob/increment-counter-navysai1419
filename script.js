38906b94911cd430e6107d9c1229a069e667486b
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-08 14:02:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+ let counterValue = 0;
+
+    function increment() {
+        // Display the un-incremented value in an alert
+        alert("The un-incremented value is: " + counterValue);
+
+        // Increment the counter value
+        counterValue++;
+
+        // Update the counter display
+        document.getElementById("counter").textContent = counterValue;
+    }
